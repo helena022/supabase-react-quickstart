721fadb4d7b5ff48113ab8c553f0d7afafcbe49f
Helena Viher <viherhelena@gmail.com>
2022-04-20 19:45:03 +0200

    development: add login and account pages

---
+++ src/Account.js
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react"
+import { supabase } from "./supabaseClient"
+
+const Account = ({ session }) => {
+  const [loading, setLoading] = useState(true)
+  const [username, setUsername] = useState(null)
+  const [website, setWebsite] = useState(null)
+  const [avatarUrl, setAvatarUrl] = useState(null)
+
+  useEffect(() => {
+    getProfile()
+  }, [session])
+
+  const getProfile = async () => {
+    try {
+      setLoading(true)
+      const user = supabase.auth.user()
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select("username, website, avatar_url")
+        .eq("id", user.id)
+        .single()
+
+      if (error && status !== 400) {
+        throw error
+      }
+
+      if (data) {
+        setUsername(data.username)
+        setWebsite(data.website)
+        setAvatarUrl(data.avatar_url)
+      }
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const updateProfile = async (e) => {
+    e.preventDefault()
+
+    try {
+      setLoading(true)
+      const user = supabase.auth.user()
+
+      const updates = {
+        id: user.id,
+        username,
+        website,
+        avatarUrl,
+        updated_at: new Date(),
+      }
+
+      let { error } = await supabase.from("profiles").upsert(updates, {
+        returning: "minimal",
+      })
+
+      if (error) {
+        throw error
+      }
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div aria-live='polite'>
+      {loading ? (
+        "Saving..."
+      ) : (
+        <form onSubmit={updateProfile} className='form-widget'>
+          <div>Email: {session.user.email}</div>
+          <div>
+            <label htmlFor='username'>Name</label>
+            <input
+              id='username'
+              type='text'
+              value={username || ""}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>Website: {session.user.website}</div>
+          <div>
+            <label htmlFor='username'>Website</label>
+            <input
+              id='website'
+              type='text'
+              value={website || ""}
+              onChange={(e) => setWebsite(e.target.value)}
+            />
+          </div>
+          <div>
+            <button
+              type='button'
+              className='button block'
+              onClick={() => supabase.auth.signOut()}
+            >
+              Sign Out
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  )
+}
+
+export default Account
